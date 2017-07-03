6c8d951a2a8f1b20b9c6afee6965a8de46ceb3d4
kelvinst <kelvin.stinghen@gmail.com>
2017-07-03 14:04:28 -0300

    Powering up put_tenant recursivity

--- priv/test_repo/tenant_migrations/20160711125401_test_create_tenant_notes.exs
+++ priv/test_repo/tenant_migrations/20160711125401_test_create_tenant_notes.exs
@@ -4,6 +4,7 @@ defmodule Triplex.TestRepo.Migrations.CreateTenantNotes do
   def change do
     create table(:notes) do
       add :body, :string
+      add :parent_id, references(:notes, on_delete: :delete_all)
     end
   end
 end
--- test/support/models/note.ex
+++ test/support/models/note.ex
@@ -4,11 +4,15 @@ defmodule Triplex.Note do
 
   schema "notes" do
     field :body, :string
+    belongs_to :parent, Triplex.Note
+    has_many :children, Triplex.Note, foreign_key: :parent_id
   end
 
   def changeset(model, params \\ %{}) do
     model
     |> cast(params, ~w(body))
+    |> cast_assoc(:parent)
+    |> cast_assoc(:children)
     |> validate_required(:body)
   end
 end

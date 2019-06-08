b50f723c563ca4a3ccf40d336a7f2ad96f741f75
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2019-06-08 15:17:16 -0300

    Remove plug dependency

--- mix.exs
+++ mix.exs
@@ -53,7 +53,6 @@ defmodule Triplex.Mixfile do
       {:excoveralls, "~> 0.10", only: :test},
       {:inch_ex, ">= 0.0.0", only: :docs},
       {:mariaex, "~> 0.9.0", optional: true},
-      {:plug, "~> 1.6", optional: true},
       {:postgrex, ">= 0.14.0", optional: true}
     ]
   end
